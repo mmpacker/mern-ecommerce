3ce5b4ee35741d5c6d626c11b07b62cb245416fb
mmpacker <mmpacker@gmail.com>
2021-06-08 10:40:19 -0400

    set up React Router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,23 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { Container } from 'react-bootstrap'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import Home from './pages/Home'
+import ProductDetails from './pages/ProductDetails'
 
 const App = () => {
   return (
-    <>
-    <Header />
-      <main className='py-3'>
-        <Container> 
-          <Home />
-        </Container>
-      </main>
-    <Footer />
-    </>
+    <Router>
+      <Header />
+        <main className='py-3'>
+          <Container> 
+            <Route path='/' component={Home} exact />
+            <Route path='/product/:id' component={ProductDetails} />
+          </Container>
+        </main>
+      <Footer />
+    </Router>
   );
 }
 
---
+++ frontend/src/pages/ProductDetails.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ProductDetails = () => {
+  return (
+    <div>
+      Product Details
+    </div>
+  )
+}
+
+export default ProductDetails
