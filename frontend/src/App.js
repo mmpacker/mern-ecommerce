98bf354bcb83cb03df8fb118143e9f09ac2759f9
mmpacker <mmpacker@gmail.com>
2021-06-08 10:04:39 -0400

    add Home page with product listing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Container } from 'react-bootstrap'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import Home from './pages/Home'
 
 const App = () => {
   return (
@@ -9,7 +10,7 @@ const App = () => {
     <Header />
       <main className='py-3'>
         <Container> 
-          <h1>Welcome to My Store!</h1> 
+          <Home />
         </Container>
       </main>
     <Footer />
