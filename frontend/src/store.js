214ee964d24a05e3794d61a43d01be96707cace7
mmpacker <mmpacker@gmail.com>
2021-06-09 14:22:47 -0400

    initial redux store set up

---
+++ frontend/src/store.js
@@ -0,0 +1,14 @@
+import FormImpl from 'react-bootstrap/esm/Form'
+import { createStore, combineReducers, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+import { composeWithDevTools } from 'redux-devtools-extension'
+
+const reducer = combineReducers({})
+
+const initialState = {}
+
+const middleware = [thunk]
+
+const store = createStore(reducer, initialState, composeWithDevTools(applyMiddleware(...middleware)))
+
+export default store
