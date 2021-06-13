ef49e306eb6951063e27f9feacda47f62c8eccd4
mmpacker <mmpacker@gmail.com>
2021-06-12 21:51:11 -0400

    add User Delete admin functionality

--- frontend/src/pages/UserList.js
+++ frontend/src/pages/UserList.js
@@ -4,7 +4,7 @@ import { Table, Button } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import Message from '../components/Message'
 import Loader from '../components/Loader'
-import { listUsers } from '../actions/userActions'
+import { listUsers, deleteUser } from '../actions/userActions'
 
 const UserList = ({ history }) => {
   const dispatch = useDispatch()
@@ -15,16 +15,21 @@ const UserList = ({ history }) => {
   const userLogin = useSelector(state => state.userLogin)
   const { userInfo } = userLogin
 
+  const userDelete = useSelector(state => state.userDelete)
+  const { success:successDelete } = userDelete
+
   useEffect(() => {
     if(userInfo && userInfo.isAdmin) {
       dispatch(listUsers())
     } else {
       history.push('/login')
     }
-  }, [dispatch, history])
+  }, [dispatch, history, successDelete])
 
   const deleteHandler = (id) => {
-    console.log('delete')
+    if (window.confirm('Are you sure you want to delete this user?')) {
+      dispatch(deleteUser(id))
+    }
   }
 
   return (
