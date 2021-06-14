aa7801633e6d80d8b9bebd69d2ca2b5806e01baa
mmpacker <mmpacker@gmail.com>
2021-06-13 21:16:17 -0400

    add front end for product reviews

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -83,18 +83,20 @@ const updateProduct = asyncHandler(async (req, res) => {
   }
 })
 
-//@desc Create new review
-//@route PUT /api/products/:id/review
-//@access Private
+// @desc    Create new review
+// @route   POST /api/products/:id/reviews
+// @access  Private
 const createProductReview = asyncHandler(async (req, res) => {
   const { rating, comment } = req.body
 
   const product = await Product.findById(req.params.id)
 
-  if(product) {
-    const alreadyReviewed = product.reviews.find(r => r.user.toString() === req.user._id.toString())
+  if (product) {
+    const alreadyReviewed = product.reviews.find(
+      (r) => r.user.toString() === req.user._id.toString()
+    )
 
-    if(alreadyReviewed) {
+    if (alreadyReviewed) {
       res.status(400)
       throw new Error('Product already reviewed')
     }
@@ -103,17 +105,19 @@ const createProductReview = asyncHandler(async (req, res) => {
       name: req.user.name,
       rating: Number(rating),
       comment,
-      user: req.user._id
+      user: req.user._id,
     }
 
     product.reviews.push(review)
 
-    product.numReviews = product.reivews.length
+    product.numReviews = product.reviews.length
 
-    product.rating = product.reviews.reduce((acc, item) => item.rating + acc, 0) / product.reviews.length
+    product.rating =
+      product.reviews.reduce((acc, item) => item.rating + acc, 0) /
+      product.reviews.length
 
     await product.save()
-    res.status(201).json({ message: 'Review Added'})
+    res.status(201).json({ message: 'Review added' })
   } else {
     res.status(404)
     throw new Error('Product not found')
