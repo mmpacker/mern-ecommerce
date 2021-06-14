b69a81e0f7ac2a005a725a4bafdc718e80b7cce0
mmpacker <mmpacker@gmail.com>
2021-06-14 09:24:15 -0400

    add Top Products carousel

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express'
 const router = express.Router()
-import { createProduct, deleteProduct, getProductById, getProducts, updateProduct, createProductReview } from '../controllers/productController.js'
+import { createProduct, deleteProduct, getProductById, getProducts, updateProduct, createProductReview, getTopProducts } from '../controllers/productController.js'
 import { protect, admin } from '../middleware/authMiddleware.js'
 
 router.route('/').get(getProducts).post(protect, admin, createProduct)
 router.route('/:id/reviews').post(protect, createProductReview)
+router.get('/top', getTopProducts)
 router.route('/:id').get(getProductById).delete(protect, admin, deleteProduct).put(protect, admin, updateProduct)
 
 export default router
